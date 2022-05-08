2bb604476a7e17e941817970321c44b2a77178c3
John Marquess <john@marquess.me>
2022-05-08 14:11:17 +1000

    Updated to match presentation

--- main_script.R
+++ main_script.R
@@ -6,6 +6,7 @@ library(janitor)
 
 data_day <- str_replace_all(today(), "-", "_")
 
+# Read a spreadhseet with a changing name from a network resource
 data_path <- paste0(
   "U:/CDU/CduImt/CDB led/2019-nCoV 2020/EPI/Case Line List - NOCS Reporting/data",
   "/",
@@ -15,25 +16,35 @@ data_path <- paste0(
   data_day,
   ".xlsx"
 )
+
+# Let's have a look
 data_path
 
+# remember to specify the worksheet by name or number
 NOCS_data <- read_xlsx("data/line_list_current_NOCS-R_2022_05_07.xlsx", sheet = 4)
+
+# glimpse - very helpful to quickly glace at all variables
 glimpse(NOCS_data)
 
 # NOCS_data %>% group_by(HOSPITALISED) %>% tally()
 
-nocs <- NOCS_data %>% filter(COLLECTDATE >= dmy("01012022"), COLLECTDATE < today()) %>% 
+
+# select, filter, and mutate
+
+nocs <- NOCS_data %>% 
+    filter(COLLECTDATE >= dmy("01012022"), COLLECTDATE < today()) %>% 
     select(NOTF_ID, COLLECTDATE, INDIG_STATUS, BIRTHDATE,SEX, STATUS, AGE_AT_ONSET, 
-           HHS, PHU,HOSPITALISED, Death, VACCINATION_STATUS, RES_LAT, RES_LONG, RESIDENCE_SA2) %>% 
+           HHS, PHU,HOSPITALISED, Death, VACCINATION_STATUS, 
+           RES_LAT, RES_LONG, RESIDENCE_SA2) %>% 
     mutate(week_date = floor_date(COLLECTDATE, "week", week_start = 6))
 
 glimpse(nocs)
 
 
-# make labels for age groups
+# make labels for age groups using a vector
 age_labels <- c("0 to 19", "20 to 39", "40 to 59", "60 to 79", "80 plus")
 
-
+# using muate to make a new age grouping variable usnig AGE_AT_ONSET
 nocs <- nocs %>% mutate(AGE_GROUP = cut(AGE_AT_ONSET,
   breaks = c(-Inf, 20, 40, 60, 80, Inf),
   labels = age_labels,
@@ -41,7 +52,11 @@ nocs <- nocs %>% mutate(AGE_GROUP = cut(AGE_AT_ONSET,
 ))
 
 
+# A handy verification check
 # nocs %>% select(AGE_AT_ONSET, AGE_GROUPS) %>% View()
+
+
+# A gentle introdcution to pivoting between wide and long data
 
 this_week <- week(today())
 n_weeks_ago <- week(today())-6
@@ -52,37 +67,38 @@ case_HHS_summary_n_wks <- nocs %>%
     tally() %>% 
     filter(between(week_num, n_weeks_ago, this_week)) %>% 
     ungroup() %>% 
-    select(-week_date) %>%
-    mutate(HHS = str_to_title(HHS)) %>% 
+    select(-week_num) %>%
+    # This is especially for Diana!!!
+    mutate(HHS = str_to_title(HHS), HHS = str_replace_all(HHS, "And", "and")) %>% 
     filter(!is.na(HHS)) %>% 
-    pivot_wider(names_from = week_num, values_from = n, values_fill = 0) %>% 
+    pivot_wider(names_from = week_date, values_from = n, values_fill = 0) %>% 
     filter(!HHS == "Unknown")
 
 case_HHS_summary_n_wks
 
-kable(case_HHS_summary_n_wks, format = "simple", align = "lrrrrrr",
+kable(case_HHS_summary_n_wks, format = "simple",
       caption = "COVID-19 case numbers per HHS over previous 6 weeks")
 
 kable(case_HHS_summary_n_wks,
       caption = "COVID-19 case numbers per HHS over previous 6 weeks") %>% 
     kable_styling(full_width = F) %>% 
-    add_header_above(c(" " = 1, "Week" = 6))
+    add_header_above(c(" " = 1, "Week" = this_week-n_weeks_ago))
 
 
 
 
-age_rate <- nocs %>%
-  select(week_date, AGE_AT_ONSET, AGE_GROUP) %>%
-  filter(!is.na(AGE_AT_ONSET)) %>%
-  group_by(week_date, AGE_GROUP) %>%
-  tally() %>%
-  pivot_wider(names_from = AGE_GROUP,
-              values_from = n,
-              values_fill = 0) %>%
-  pivot_longer(cols = 2:6, names_to = "AgeGroup", values_to = "Notifications") %>%
-    ungroup() %>% 
-    filter(year(week_date) == 2022)
-
-age_rate %>% 
-    ggplot(. , aes(x = week_date, y = Notifications, fill=AgeGroup)) +
-    geom_col() + theme_minimal()
+# age_rate <- nocs %>%
+#   select(week_date, AGE_AT_ONSET, AGE_GROUP) %>%
+#   filter(!is.na(AGE_AT_ONSET)) %>%
+#   group_by(week_date, AGE_GROUP) %>%
+#   tally() %>%
+#   pivot_wider(names_from = AGE_GROUP,
+#               values_from = n,
+#               values_fill = 0) %>%
+#   pivot_longer(cols = 2:6, names_to = "AgeGroup", values_to = "Notifications") %>%
+#     ungroup() %>% 
+#     filter(year(week_date) == 2022)
+# 
+# age_rate %>% 
+#     ggplot(. , aes(x = week_date, y = Notifications, fill=AgeGroup)) +
+#     geom_col() + theme_minimal()
--- school_reports.R
+++ school_reports.R
@@ -6,12 +6,16 @@ library(RColorBrewer)
 library(tidyquant)
 
 
-# Load the data
+# Loading the data
 rat <- read_xlsx("data/School_Cohort_PCR_RAT_Plots_Preschool.xlsx", sheet = 7) %>% 
     select(1:4) 
 pcr <- read_xlsx("data/School_Cohort_PCR_RAT_Plots_Preschool.xlsx", sheet = 5) %>% 
     select(c(1,2,3,5))
 
+# Data frames
+
+
+# Using vectors
 colnames(pcr) <- c("DATE", "NOCS_PRIMARY", "NOCS_SECONDARY", "NOCS_PRESCHOOL")
 colnames(rat) <- c("DATE", "RAT_PRIMARY", "RAT_SECONDARY", "RAT_PRESCHOOL")
 
@@ -19,17 +23,48 @@ all_results <- full_join(rat, pcr, by = "DATE") %>%
     filter(year(DATE) > 2021)
 
 all_results_long <- all_results %>% 
-    pivot_longer(cols = 2:7, names_to = "Cohort", values_to = "Notifications")
+    pivot_longer(cols = 2:7, names_to = "Cohort", values_to = "Notifications") %>% 
+    mutate(type = as_factor(Cohort)) %>% 
+    mutate(groups = recode(Cohort, 
+                           NOCS_PRESCHOOL = "Children < 5 (NoCS)",
+                           NOCS_PRIMARY = "Children 5 to 11 (NoCS)",
+                           NOCS_SECONDARY = "Children 12 to 17 (NoCS)",
+                           RAT_PRESCHOOL = "Children < 5 (RATs)",
+                           RAT_PRIMARY = "Children 5 to 11 (RATs)",
+                           RAT_SECONDARY = "Children 12 to 17 (RATs)"))
 
 
 all_results_long %>% 
     filter(DATE > dmy("01032022")) %>% 
+    select(-Cohort) %>% 
+    mutate(Cohort = groups) %>% 
     ggplot(., aes(x = DATE, y = Notifications, color = Cohort)) + 
-    # geom_line() +
-    geom_point(alpha = 0.5) +
+    geom_point(alpha = 0.4, size = 2) +
     # stat_smooth(se = FALSE) +
     theme_fivethirtyeight() +
     scale_color_brewer(palette = "Paired") +
     geom_ma(ma_fun = SMA, n = 7, size = 1, linetype = "solid", alpha = 0.6) + 
     ggtitle("PCT and RAT cases in children", subtitle = "Solid lines are 7-day moving average")
 
+
+rats_and_nocs <- all_results %>% 
+    mutate(all_under_5 = RAT_PRESCHOOL + NOCS_PRESCHOOL,
+           all_5_to_11 = RAT_PRIMARY + NOCS_PRIMARY,
+           all_12_to_17 = RAT_SECONDARY + NOCS_SECONDARY) %>% 
+    select(1, 8,9,10) 
+
+rats_and_nocs_long <- rats_and_nocs %>% 
+    pivot_longer(cols = 2:4, names_to = "Cohort", values_to = "Notifications") %>% 
+    mutate(Cohort = recode(Cohort, 
+                           all_under_5 = "Children < 5",
+                           all_5_to_11 = "Children 5 to 11",
+                           all_12_to_17 = "Children 12 to 17"))
+
+
+rats_and_nocs_long %>% 
+    ggplot(., aes(x = DATE, y = Notifications, color = Cohort)) + 
+    theme_fivethirtyeight() +
+    scale_color_brewer(palette = "Set2") +
+    geom_point(alpha = 0.3, size = 2, stroke=1) +
+    geom_ma(ma_fun = SMA, n = 7, size = 1.2, linetype = "solid") + 
+    ggtitle("COVID-19 cases in children (NoCS and RATS combined)", subtitle = "Solid lines are 7-day moving average")
